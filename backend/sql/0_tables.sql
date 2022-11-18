55802820b1e9182f39d711aa7edefc53b98c8a9c
LeFrosch <daniel-brauner@outlook.com>
2022-11-19 00:46:23 +0100

    [add] database init

---
+++ backend/sql/0_tables.sql
@@ -0,0 +1,39 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS "user" (
+    "id"            uuid            PRIMARY KEY DEFAULT uuid_generate_v4(),
+    "name"          VARCHAR (50)    NOT NULL UNIQUE,
+    "balance"       INT             NOT NULL DEFAULT 0 CHECK ("balance" >= 0)
+);
+
+CREATE TABLE IF NOT EXISTS "organization" (
+    "id"            uuid            PRIMARY KEY DEFAULT uuid_generate_v4(),
+    "name"          VARCHAR (50)    NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS "security" (
+    "id"            uuid            PRIMARY KEY DEFAULT uuid_generate_v4(),
+    "name"          VARCHAR (50)    NOT NULL UNIQUE,
+    "description"   TEXT            NOT NULL,
+    "creator"       uuid            NOT NULL REFERENCES "organization" ("id"),
+    "creation_date" DATE            NOT NULL DEFAULT now(),
+    "ttl_1"         DATE            NOT NULL CHECK ("ttl_1" > "creation_date"),
+    "ttl_2"         DATE            NOT NULL CHECK ("ttl_2" > "ttl_1")
+);
+
+CREATE TABLE IF NOT EXISTS "order" (
+    "id"            uuid            PRIMARY KEY DEFAULT uuid_generate_v4(),
+    "type"          BOOLEAN         NOT NULL,
+    "security"      uuid            NOT NULL REFERENCES "security" ("id"),
+    "quantity"      INT             NOT NULL CHECK ("quantity" > 0),
+    "price"         INT             NOT NULL CHECK ("price" > 0),
+    "side"          BOOLEAN         NOT NULL,
+    "user"          uuid            NOT NULL REFERENCES "user" ("id"),
+    "creation_date" DATE            NOT NULL DEFAULT now()
+);
+
+CREATE TABLE IF NOT EXISTS "matches" (
+    "id"            uuid            PRIMARY KEY DEFAULT uuid_generate_v4(),
+    "buyer"         uuid            NOT NULL REFERENCES "order" ("id"),
+    "seller"        uuid            NOT NULL REFERENCES "order" ("id")
+);
