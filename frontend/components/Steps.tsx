43479f91bf45c8d07d81c1171b29236e8d62e396
fried16 <friedlaender.nicholas@guj.de>
2022-11-19 15:55:22 +0100

    asdads

--- frontend/components/Steps.tsx
+++ frontend/components/Steps.tsx
@@ -1,14 +1,20 @@
 export default function Setps() {
   let test = 1;
-  let test1 = 2;
+  let test1 = 1;
   return (
-    <div>
+    <div className="pt-5 pr-20">
       <h2 className="sr-only">Steps</h2>
 
       <div className="relative after:absolute after:inset-x-0 after:top-1/2 after:block after:h-0.5 after:-translate-y-1/2 after:rounded-lg after:bg-gray-100">
         <ol className="relative z-10 flex justify-between text-sm font-medium text-gray-500">
           <li className="flex items-center bg-white p-2">
-            <span className="h-6 w-6 rounded-full bg-gray-100 text-center text-[10px] font-bold leading-6">
+            <span
+              className={`${
+                test + test1 >= 2
+                  ? "h-6 w-6 rounded-full bg-blue-600 text-center text-[10px] font-bold leading-6 text-white"
+                  : "h-6 w-6 rounded-full bg-gray-100 text-center text-[10px] font-bold leading-6"
+              }  `}
+            >
               1
             </span>
 
@@ -18,7 +24,7 @@ export default function Setps() {
           <li className="flex items-center bg-white p-2">
             <span
               className={`${
-                test + test1 == 2
+                test + test1 >= 3
                   ? "h-6 w-6 rounded-full bg-blue-600 text-center text-[10px] font-bold leading-6 text-white"
                   : "h-6 w-6 rounded-full bg-gray-100 text-center text-[10px] font-bold leading-6"
               }  `}
@@ -30,7 +36,13 @@ export default function Setps() {
           </li>
 
           <li className="flex items-center bg-white p-2">
-            <span className="h-6 w-6 rounded-full bg-gray-100 text-center text-[10px] font-bold leading-6">
+            <span
+              className={`${
+                test + test1 >= 4
+                  ? "h-6 w-6 rounded-full bg-blue-600 text-center text-[10px] font-bold leading-6 text-white"
+                  : "h-6 w-6 rounded-full bg-gray-100 text-center text-[10px] font-bold leading-6"
+              }  `}
+            >
               3
             </span>
 
---
+++ frontend/query.ts
@@ -0,0 +1,11 @@
+import { QueryClient } from "@tanstack/react-query";
+
+export default function query() {
+  const query = new QueryClient();
+}
+// GET user history
+
+// GET all history
+// GET profolio value
+// POST buy order
+// POST sell order
